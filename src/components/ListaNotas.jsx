7462494902086470d705a89dd569f6152272080f
pacificstate01 <javier.otarola@inacapmail.cl>
2024-07-17 16:58:50 -0400

    texto agregado en caso de no haber notas

--- src/components/ListaNotas.jsx
+++ src/components/ListaNotas.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {Notas} from './Notas'; 
 
 export const ListaNotas = ({ notas,onDelete }) => {
+  if (notas.length === 0) {
+    return <h1>No hay notas</h1>;
+  }
   return (
     <div className="d-flex flex-wrap justify-content-between">
       {notas.map((nota, index) => (
